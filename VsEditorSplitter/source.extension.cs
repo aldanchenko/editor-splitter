fe990970f1afe5ab9c62a1c395066edefef9fbba
Aleksandr <aldanchenko@gmail.com>
2022-02-08 14:50:23 +0200

    feat: add vsix description as class

---
+++ VsEditorSplitter/source.extension.cs
@@ -0,0 +1,13 @@
+namespace VsEditorSplitter
+{
+    internal sealed partial class Vsix
+    {
+        public const string Id = "VsEditorSplitter.a668705c-aa73-4423-a001-ace157c84f5c";
+        public const string Name = "VsEditorSplitter";
+        public const string Description = @"Empty VSIX Project.";
+        public const string Language = "en-US";
+        public const string Version = "1.0.0";
+        public const string Author = "Alex Danchenko";
+        public const string Tags = "vsix, tool";
+    }
+}
